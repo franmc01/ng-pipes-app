48cdb92d15c45121b341ba9cea4f21a4031a2972
franmc01 <francmarinc@gmail.com>
2025-04-02 22:35:03 -0500

    date, currency, number pipes

--- src/app/pages/basic/basic.component.ts
+++ src/app/pages/basic/basic.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-basic',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic.component.html',
   styleUrl: './basic.component.css'
 })
 export class BasicComponent {
+  nameLower = signal("francisco")
+  nameUpper = signal("Francisco")
+  fullName = signal("FrAnCiSCo")
+
+  customDate = signal(new Date())
+
+  tick = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+    }, 1000)
+
+    onCleanup(() => clearInterval(interval))
+  })
 
 }
--- src/app/pages/numbers/numbers.component.ts
+++ src/app/pages/numbers/numbers.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
-  imports: [],
+  imports: [DecimalPipe, CurrencyPipe, PercentPipe],
   templateUrl: './numbers.component.html',
   styleUrl: './numbers.component.css'
 })
 export class NumbersComponent {
-
+  totalSells = signal(935978353.34554)
+  percent = signal(0.345345)
 }
